9f17613f3d7442b5e32e6114e587e5355cb34163
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-04-21 15:44:45 +0200

    Update CException.cpp

--- ProjetCPPGraphe/ProjetCPPGraphe/CException.cpp
+++ ProjetCPPGraphe/ProjetCPPGraphe/CException.cpp
@@ -1,38 +1,64 @@
-#include <string>
+///
+/// @brief Voir CException.h pour les descriptions des methodes
+/// @file CEsxeption.cpp
+/// @author NASSIRI Adam
+/// @coauthor BLUMSTEIN Thomas
+/// @date 2021-04/21
+///
+
+#include <fstream>
 #include <iostream>
-
 #include "CException.h"
+using namespace std;
 
-
-CException::CException()
-{
+CException::CException() {
+	pEXCmessage = (char*) "Exception innatendue";
+	cEXCoperation = '0';
 }
 
-CException::CException(char cEXCope, char* pEXCmess)
+CException::CException(char cEXCope, char * pEXCmess)
 {
+	pEXCmessage = pEXCmess;
+	cEXCoperation = cEXCope;
 }
 
-CException::~CException()
-{
+CException::~CException() {
+
 }
 
-void CException::EXCGetMessage(char* pEXCmess)
-{
+/// @brief retourne le message d exception de la CException
+/// @param RIEN
+/// @return sEXCMessage le message d exception
+void CException::EXCGetMessage(char* pEXCmess) {
+	strcpy_s(pEXCmess, sizeof pEXCmess, pEXCmessage);
 }
 
-void CException::EXCSetMessage(char* pEXCmess)
-{
+/// @brief definit le message d exception dans la CException
+/// @param smessage le nouveau message d exception
+/// @return RIEN
+void CException::EXCSetMessage(char* pEXCmess) {
+	pEXCmessage = pEXCmess;
 }
 
-char CException::EXCGetOperation()
-{
-	return 0;
+/// @brief retourne l operation concernee par la CException
+/// @param RIEN
+/// @return cEXCOperation l operateur sous forme de char
+char CException::EXCGetOperation() {
+	return cEXCoperation;
 }
 
-void CException::EXCSetOperation(char cEXCope)
-{
+/// @brief definit l operation concernee par la CException
+/// @param cOperation l operateur sous forme de char
+/// @return RIEN
+void CException::EXCSetOperation(char cEXCope) {
+	cEXCoperation = cEXCope;
 }
 
-void CException::EXCAfficherException()
-{
+/// @brief affiche le message d'exception de la CException courante
+/// @param RIEN
+/// @return RIEN
+void CException::EXCAfficherException() {
+	cout << "Operation " << cEXCoperation << ", exception: " << pEXCmessage << endl;
 }
+
+
